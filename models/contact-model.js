a70be6ed58d249d78cecbc604635f7dae00ca895
ravitej199 <75783352+ravitej199@users.noreply.github.com>
2024-04-20 16:55:03 +0530

    Add files via upload

---
+++ models/contact-model.js
@@ -0,0 +1,12 @@
+import mongoose  from "mongoose";
+
+
+const contactSchema = new mongoose.Schema({
+    username : {type : String, required : true},
+    email : {type : String, required: true},
+    message : {type : String, required : true},
+});
+
+
+const Contact = new mongoose.model('Contact', contactSchema);
+export default Contact;
---
+++ models/user-model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        require : true,
+    },
+    email :{
+        type : String,
+        require: true, 
+    },
+    phone : {
+        type : String,
+        require : true,
+    },
+    password : {
+        type : String,
+        require : true,
+    },
+    isAdmin:{
+        type : Boolean,
+        default: false,
+    }
+})
+
+
+userSchema.pre('save',async function(next){
+ const user = this;
+ if(!user.isModified("password")){
+    next()
+ }
+ try{
+    const saltRound = await bcrypt.genSalt(10);
+    const hash_pswd = await bcrypt.hash(user.password,saltRound);
+    user.password = hash_pswd;
+ }catch(error){
+    next(error)
+ }
+})
+
+userSchema.methods.generateToken =async function () {
+  try{
+
+    return jwt.sign({
+        userId: this._id.toString(),
+        email : this.email,
+        isAdmin : this.isAdmin,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+        expiresIn:"1m",
+    }
+    );
+
+  }catch(error){
+    console.error(error);
+  }
+
+};
+
+
+
+const User = new mongoose.model('User',userSchema)
+
+export default User;
